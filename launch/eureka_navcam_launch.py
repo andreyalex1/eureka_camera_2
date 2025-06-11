19495f6feadaf527c3b2aefad1cccbc67e2bbfdb
andreyalex1 <andrey040902@gmail.com>
2025-06-11 19:45:30 +0300

    Automated commit: sync local changes

--- launch/eureka_navcam_launch.py
+++ launch/eureka_navcam_launch.py
@@ -3,49 +3,85 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-import os
+
+
+rs_dir = get_package_share_directory('realsense2_camera')
 
 def generate_launch_description():
-    json_config_path = os.path.join(
-        get_package_share_directory('eureka_camera_2'),
-        'launch',
-        'real.json'
-    )
-
     return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(
-                    get_package_share_directory('realsense2_camera'),
-                    'launch',
-                    'rs_launch.py'
-                )
-            ),
+       IncludeLaunchDescription(
+            launch_description_source = PythonLaunchDescriptionSource(rs_dir + '/launch/rs_launch.py'),
             launch_arguments={
-                # Основные настройки
-                'device_type': 'd435i',  
-                'enable_depth': 'true',  
-                'enable_color': 'true',  
-                'align_depth.enable': 'true',  
-                'enable_sync': 'true',
-                'enable_accel': 'true',
-                'enable_infra1': 'false',  
-                'enable_infra2': 'false',
-                'enable_pointcloud': 'true',  
-                'pointcloud.allow_no_texture_points': 'true',  
-                'spatial_filter.enable': 'true',  
-                'spatial_filter.filter_magnitude': '1.0',
-                'spatial_filter.filter_smooth_alpha': '0.54',
-                'spatial_filter.filter_smooth_delta': '7.0',
-                'hole_filling_filter.enable': 'true',  
-                'temporal_filter.enable': 'false',  
-                'disparity_filter.enable': 'false',  
-                'decimation_filter.enable': 'false',                  
-                'tf_publish_rate': '10.0',  
-                'depth_fps': '15',  
-                'color_fps': '15', 
-                'json_file_path': 'src/eureka_camera_2/launch/real.json',                
-                'initial_reset': 'false',  
-            }.items()
-        )
+                            'device_type' : 'd435',
+                            'pointcloud.enable' : 'true', 
+                            'pointcloud.allow_no_texture_points' : 'true',
+                            'unite_imu_method' :  '2', 
+                            'enable_gyro': 'true', 
+                            'enable_accel': 'true',
+                            'tf_publish_rate': '10',
+                            'hdr_merge.enable' : 'true',
+                            # 'align_depth.enable':'true'
+                            'decimation_filter.enable' : 'false',#maybe
+                            'spatial_filter.enable' : 'true',  
+                            'spatial_filter.filter_magnitude' : '1.0',
+                            'spatial_filter.filter_smooth_alpha': '0.54',
+                            'spatial_filter.filter_smooth_delta': '7.0', 
+                            'temporal_filter.enable' :   'false',
+                            'temporal_filter.filter_smooth_alpha': '1.0',
+                            'temporal_filter.filter_smooth_delta': '100.0',
+                            'disparity_filter.enable' :  'false',
+                            'hole_filling_filter.enable' : 'true',
+                            'hdr_merge.enable' : 'true',
+                            'disparity_to_depth.enable': 'true',
+                            'json_file_path': 'src/eureka_camera_2/launch/real.json',
+
+          #                  'initial_reset' : 'true'
+                            }.items()
+        ),
+   #     Node(
+   #         package='usb_cam',
+   #         executable='usb_cam_node_exe',
+   #         name='hazcam',
+   #         namespace='hazcam',
+   #         output="screen",
+   #         parameters=["/home/eurekanuc/ros2_ws/src/eureka_camera_2/eureka_camera_2/hazcam_parameters.yaml"]
+  #      ),
+      #  Node(
+     #       package='usb_cam',
+    #        executable='usb_cam_node_exe',
+     #       name='pancam',
+     #       namespace='pancam',
+        #    output="screen",
+     #       parameters=["/home/eurekanuc/ros2_ws/src/eureka_camera_2/eureka_camera_2/pancam_parameters.yaml"]
+     #   ),
+     #   Node(
+      #      package='usb_cam',
+     #       executable='usb_cam_node_exe',
+      #      name='topdowncam',
+      #      namespace='topdowncam',
+       #     output="screen",
+       #     parameters=["/home/eurekanuc/ros2_ws/src/eureka_camera_2/eureka_camera_2/topdowncam_parameters.yaml"]
+      #  ),
+ #       Node(
+ #           package='usb_cam',
+ #           executable='usb_cam_node_exe',
+ #           name='armcam',
+ #           namespace='armcam',
+ #           output="screen",
+ #           parameters=["/home/eurekanuc/ros2_ws/src/eureka_camera_2/eureka_camera_2/armcam_parameters.yaml"]
+ #       ),
+ #       Node(
+ #           package='eureka_camera_2',
+ #           executable='opencv_streamer',
+ #           name='opencv_streamer',
+ #           shell=True,
+ #       ),
+ #       Node(
+ #           package='usb_cam',
+ #           executable='usb_cam_node_exe',
+ #           name='armncam',
+  #          namespace='armcam',
+  #          parameters=["/home/eurekanuc/ros2_ws/src/eureka_camera_2/eureka_camera_2/armcam_parameters.yaml"]
+  #     ),
+        
     ])
