51aaee5adab3d3232a7811dff43994638649d1c4
andreyalex1 <andrey040902@gmail.com>
2025-09-11 18:48:51 +0300

    Automated commit: sync local changes

--- launch/eureka_camera_launch.py
+++ launch/eureka_camera_launch.py
@@ -9,24 +9,6 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     return LaunchDescription([
-  #     IncludeLaunchDescription(
-  #          launch_description_source = PythonLaunchDescriptionSource(rs_dir + '/launch/rs_launch.py'),
-  #          launch_arguments={
-  #                          'pointcloud.enable' : 'true', 
-  #                          'unite_imu_method' :  '2', 
-  #                          'enable_gyro': 'true', 
-  #                          'enable_accel': 'true',
-  #        #                  'initial_reset' : 'true'
-  #                          }.items()
-  #      ),
-  #      Node(
-   #         package='usb_cam',
-  #          executable='usb_cam_node_exe',
-  #          name='hazcam',
-  #          namespace='hazcam',
-    #        output="screen",
-   #         parameters=["/home/eurekanuc/ros2_ws/src/eureka_camera_2/eureka_camera_2/hazcam_parameters.yaml"]
-#        ),
         Node(
             package='usb_cam',
             executable='usb_cam_node_exe',
@@ -67,15 +49,14 @@ def generate_launch_description():
       #      output="screen",
             parameters=["/home/eurekajetson/ros2_ws/src/eureka_camera_2/eureka_camera_2/armcam_parameters.yaml"]
         ),
-  #      Node(
-   #         package='usb_cam',
-   #         executable='usb_cam_node_exe',
-  #          name='ircam',
-   #         namespace='ircam',
-   #         respawn=True,
-  #          respawn_delay=10,
-  #    #      output="screen",
-  #          parameters=["/home/eurekajetson/ros2_ws/src/eureka_camera_2/eureka_camera_2/ircam_parameters.yaml"]
-  #      ),
-        
+        Node(
+            package='usb_cam',
+            executable='usb_cam_node_exe',
+            name='ircam',
+            namespace='ircam',
+            respawn=True,
+            respawn_delay=10,
+      #      output="screen",
+            parameters=["/home/eurekajetson/ros2_ws/src/eureka_camera_2/eureka_camera_2/ircam_parameters.yaml"]
+        ),
     ])
